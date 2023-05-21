65beba25209248a01d84d454be3d9547918d61a0
rahulr2109 <115545692+rahulr2109@users.noreply.github.com>
2023-05-21 16:21:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ with st.sidebar:
                            'Heart Disease Prediction',
                            'Parkinsons Prediction'],
                           icons=['activity','heart','person'],
-                          default_index=0)
+                          default_index=0,
+                          orientation='horizontal')
     
     
 # Diabetes Prediction Page
